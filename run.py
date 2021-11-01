13e8b6f4942c5b9b23d4681f4a16880216c013ae
mwarddev <martinward1981@googlemail.com>
2021-11-01 04:30:37 +0000

    Add function to get user credentials and a function to validate user email address

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
-import gspread 
+import gspread
 from google.oauth2.service_account import Credentials
+import re
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,8 +13,46 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("monty's_inn")
 
-bookings = SHEET.worksheet('bookings')
+def get_user_creds():
+    """
+    Get and store user name and email address for use in booking
+     and cancellation process
+    """
 
-data = bookings.get_all_values()
+    print("\nPlease enter your first name.\n")
+    fname = input("First Name: ")
+    print("\nThank you.")
+    print("Please enter your last name.\n")
+    lname = input("Last Name: ")
+    print("\nThank you.")
 
-print(data)
+    while True:
+        print("Please enter your email address.\n")
+        email = input("Email: ")
+
+        if validate_email(email):
+            print("\nThank you. Your email address is valid.")
+            break
+
+    return fname, lname, email
+
+
+def validate_email(email):
+    """
+    Try block checks email address validation using regex and raises a 
+    ValueError if email address doesn't meet requirements.
+    """
+
+    try:
+        pattern = r"([\w\d\.-]+)@([\w\d\.-]+)(\.[\w\.]+)"
+        match = re.search(pattern, email)
+        if match:
+            return True
+        else:
+            raise ValueError(f"{email} is not valid")
+    except ValueError as e:
+        print(f"Unfortunately the email address you provided {e}. \nPlease provide a valid email address.\n")
+        return False
+
+
+get_user_creds()
